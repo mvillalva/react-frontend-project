7a8b9f393b382a3ec5df85d2820196d91201c35c
mvillalva <maxi.villalva@gmail.com>
2022-10-31 19:45:13 -0300

    Id de perfil con useParams

--- team4/src/components/profile-pack/profile-edit/ProfileEdit.js
+++ team4/src/components/profile-pack/profile-edit/ProfileEdit.js
@@ -1,32 +1,36 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import './ProfileEdit.css'
 
-const ProfileEdit = () => {
-  return (
-    <div className="profiles-gate-container">
-        <div className="centered-div list-profiles-container">
-            <div className="profile-actions-container">
-                <h1>Editar perfil</h1>
-                <div className="profile-metadata profile-entry">
-                    <div className="main-profile-avatar">
-                        <div className="avatar-box">
-                            <img src="https://occ-0-7118-2773.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABWw0vRAvv1VCQh43eIGGbOU9YGB0c_wGM1K8LDuUnYfB0jVIwRW1gTpYldIoy67LtH1Ky8cxhC_mjBb6C4qfcJWHzLAJasWAXuNR.png?r=ce4" alt="" className="ltr-1etmbqw" />                    
+const ProfileEdit = (props) => {
+    const params = useParams()
+    const profile = props.profiles.filter(e => e.id === parseInt(params.id))
+
+    return (
+        <div className="profiles-gate-container">
+            <div className="centered-div list-profiles-container">
+                <div className="profile-actions-container">
+                    <h1>Editar perfil</h1>
+                    <div className="profile-metadata profile-entry">
+                        <div className="main-profile-avatar">
+                            <div className="avatar-box">
+                                <div className={'profile-icon ' + profile[0].bg}></div>                                
+                            </div>
+                        </div>
+                        <div className="profile-edit-parent">
+                            <div className="profile-edit-inputs">
+                                <label htmlFor="profile-name-entry" id="profile-name-entry-label" className="visually-hidden">Nombre del perfil</label>
+                                <input type="text" className="" id="profile-name-entry" aria-labelledby="profile-name-entry-label" placeholder="Nombre" value={profile[0].name} />
+                            </div>                
                         </div>
                     </div>
-                    <div className="profile-edit-parent">
-                        <div className="profile-edit-inputs">
-                            <label for="profile-name-entry" id="profile-name-entry-label" className="visually-hidden">Nombre del perfil</label>
-                            <input type="text" className="" id="profile-name-entry" aria-labelledby="profile-name-entry-label" placeholder="Nombre" value="User 3" />
-                        </div>                
-                    </div>
+                    <a href='/ManageProfiles' className="profile-button preferred-action">Guardar</a>
+                    <a href='/ManageProfiles' className="profile-button">Cancelar</a>
+                    <a href={'/DeleteProfile/' + profile[0].id} className="profile-button">Eliminar perfil</a>
                 </div>
-                <a href='/ManageProfiles' className="profile-button preferred-action">Guardar</a>
-                <a href='/ManageProfiles' className="profile-button">Cancelar</a>
-                <a href='/DeleteProfile' className="profile-button">Eliminar perfil</a>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProfileEdit
