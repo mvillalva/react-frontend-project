a3f6d06a6b2715853478af9e5cb684a853d26413
mvillalva <maxi.villalva@gmail.com>
2022-11-01 15:49:37 -0300

    Modificacion CSS

--- team4/src/components/profile-pack/profile-delete/ProfileDelete.js
+++ team4/src/components/profile-pack/profile-delete/ProfileDelete.js
@@ -7,23 +7,23 @@ const ProfileDelete = (props) => {
     const profile = props.profiles.filter(e => e.id === parseInt(params.id))
 
     return (
-        <div className="profiles-gate-container">
-            <div className="centered-div list-profiles-container">
-                <div className="profile-actions-container">
-                    <h1 className="h1">¿Eliminar perfil?</h1>
-                    <div className="profile-metadata profile-entry">
-                        <div className="main-profile-avatar">
-                            <div className={'profile-icon sz-img ' + profile[0].bg}></div>                            
+        <div className="delete-container">
+            <div className="centered-div animate-container">
+                <div className="delete-actions-container">
+                    <h1 className="hh1">¿Eliminar perfil?</h1>
+                    <div className="delete-metadata delete-entry">
+                        <div className="profile-avatar">
+                            <div className={'profile-icon ' + profile[0].bg}></div>
                             <div className="profile-name">{profile[0].name}</div>
                         </div>
-                        <div className="profile-delete-warning">
+                        <div className="delete-delete-warning">
                             El historial de este perfil, incluidas Mi lista, los votos y la actividad, se eliminará definitivamente, por lo que no tendrás acceso a ellos después.
                         </div>
                     </div>
-                    <a href={"/EditProfile/" + profile[0].id} role="button" tabindex="0" className="profile-button preferred-action">
+                    <a href={"/EditProfile/" + profile[0].id} className="delete-button preferred-action">
                         <span>Guardar perfil</span>
                     </a>
-                    <a href="/ManageProfiles" role="button" tabindex="0" className="profile-button">
+                    <a href="/ManageProfiles" className="delete-button">
                         <span>Eliminar perfil</span>
                     </a>
                 </div>
