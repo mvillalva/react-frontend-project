5c891031884d3f043faebb14b4441cd3b38d9650
Dario Fernando Argañaraz <dfarganaraz@agip.gov.ar>
2022-11-07 12:09:43 -0300

    Se agrega a modo de prueba un const de un array con peliculas que se muestran a traves del componente playlist

--- team4/src/components/playlist/Playlist.js
+++ team4/src/components/playlist/Playlist.js
@@ -3,9 +3,17 @@ import './Playlist.css'
 
 const Playlist = (props) => {
 
+  const myList = props.data.map((element) => {
+    return (<div className="card p-2"> <img src={element.posterUrl} alt={element.title} width={200} /></div>)
+  });
+
   return (
-    <div className='playlist'>
+    <div>
       <h1>Mi lista</h1>
+      <div className="playlist d-flex flex-row flex-wrap gap-4">
+
+        {myList}
+      </div>
     </div>
   )
 }
--- team4/src/pages/playlistPage/PlaylistPage.js
+++ team4/src/pages/playlistPage/PlaylistPage.js
@@ -2,13 +2,42 @@ import React from "react";
 import './PlaylistPage.css';
 import Playlist from "../../components/playlist/Playlist";
 
-const PlaylistPage = ({title}) => {
+const movieData = [
+    {
+        "id": 1,
+        "title": "The Cotton Club",
+        "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5ODAyNzA4OV5BMl5BanBnXkFtZTcwNzYwNTIzNA@@._V1_SX300.jpg"
+    },
+  
+    {
+        "id": 2,
+        "title": "Crocodile Dundee",
+        "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg0MTU1MTg4NF5BMl5BanBnXkFtZTgwMDgzNzYxMTE@._V1_SX300.jpg"
+    },
+    {
+        "id": 3,
+        "title": "Ratatouille",
+        "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX300.jpg"
+    },
+    {
+        "id": 4,
+        "title": "City of God",
+        "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX300.jpg"
+    },
+    {
+        "id": 5,
+        "title": "Stardust",
+        "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjkyMTE1OTYwNF5BMl5BanBnXkFtZTcwMDIxODYzMw@@._V1_SX300.jpg"
+    }
+  ];
+
+const PlaylistPage = () => {
+    
 
      return (        
           <div className="playlist-page-container">
               <div className="centered-div">
-               <Playlist title={title}>
-               </Playlist>
+               <Playlist data={movieData} />
               </div>
           </div>
       )
