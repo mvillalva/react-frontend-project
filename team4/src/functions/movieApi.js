0c0b021317640c4ad5059921d3cd2eb3b4e44988
Maximiliano Villalva <67973827+mvillalva@users.noreply.github.com>
2022-11-11 10:05:16 -0300

    Update movieApi.js

--- team4/src/functions/movieApi.js
+++ team4/src/functions/movieApi.js
@@ -1,6 +1,6 @@
 export const buscar = (peli, setTitulos) => {
     if (peli) {
-        const api_url = `http://www.omdbapi.com/?i=tt3896198&apikey=9c000cc8&s=${peli}`;
+        const api_url = `https://www.omdbapi.com/?i=tt3896198&apikey=9c000cc8&s=${peli}`;
 
         fetch(api_url)
         .then(data => data.json())
