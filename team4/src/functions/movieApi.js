8834cde73cab20a43800c831a55067102f96ec04
teotronic5438 <42853523+teotronic5438@users.noreply.github.com>
2022-11-14 09:04:26 -0300

    cambio de servidor peliculas tmdb  y keys (#39)

--- team4/src/functions/movieApi.js
+++ team4/src/functions/movieApi.js
@@ -1,14 +1,17 @@
+const APY_KEY = process.env.REACT_APP_TMDB_APYKEY;
+
 export const buscar = (peli, setTitulos) => {
     if (peli) {
-        const api_url = `https://www.omdbapi.com/?i=tt3896198&apikey=9c000cc8&s=${peli}`;
-
+        // const api_url = `https://www.omdbapi.com/?i=tt3896198&apikey=9c000cc8&s=${peli}`;
+        const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${APY_KEY}&language=es-ES&query=${peli}&page=1&include_adult=false`;
         fetch(api_url)
         .then(data => data.json())
         .then(resultado => {
-            const {Search=[]} = resultado;
+            // const {Search=[]} = resultado;
+            const {results=[]} = resultado;
             setTitulos({
-            total: Search.length,
-            arreglo: Search,
+            total: results.length,
+            arreglo: results,
             busqueda: true,
             });
         })
