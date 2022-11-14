233fee3e072021b999f20872fe1f6a0ef5cd1034
teotronic5438 <elias.orihuela@gmail.com>
2022-11-14 08:34:08 -0300

    cambio de servidor peliculas tmdb  y keys

--- team4/src/components/ListadoBusqueda/ListadoBusqueda.js
+++ team4/src/components/ListadoBusqueda/ListadoBusqueda.js
@@ -3,6 +3,8 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+const BASE_IMG = process.env.REACT_APP_BASE_URL_IMG;
+
 export default function ListadoBusqueda(props){
     const {resultados} = props;
     console.log(props);
@@ -10,13 +12,13 @@ export default function ListadoBusqueda(props){
         return(
             <Col key={index}>
                 <Card className='title-card'>
-                <Card.Img variant="top" src={resultado.Poster} alt="Poster pelicula" />
+                <Card.Img variant="top" src={BASE_IMG + resultado.poster_path} alt="Poster pelicula" />
                 {/* <Card.Body>
-                    <Card.Title>{resultado.Title}</Card.Title>
-                    <Card.Text>
-                    Año de estreno: {resultado.Year} - Categoria: {resultado.Type}
-                    </Card.Text>
-                </Card.Body> */}
+                    {/* <Card.Title>{resultado.Title}</Card.Title> */}
+                    {/* <Card.Text> */}
+                    {/*Año de estreno: {resultado.Year} - Categoria: {resultado.Type} */}
+                    {/* </Card.Text> */}
+                {/*</Card.Body> */}
                 </Card>
             </Col>
 
