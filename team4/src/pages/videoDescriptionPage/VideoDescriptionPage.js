8416b5868063bfd16502b450b036dfb5a5f6202b
santifigueroa <sfigueroa@modernizacion.gob.ar>
2022-11-28 14:40:36 -0300

    Mover funcionalidad y estilos de Modal dentro de VideoDescriptionPage component y hacer ajustes varios

--- team4/src/pages/videoDescriptionPage/VideoDescriptionPage.js
+++ team4/src/pages/videoDescriptionPage/VideoDescriptionPage.js
@@ -1,27 +1,29 @@
 import React from "react";
 import './VideoDescriptionPage.css';
 import VideoDescription from "../../components/video-description/VideoDescription";
-
+import Modal from 'react-bootstrap/Modal';  
 
 import { useEffect, useState } from "react";
 
 const APY_KEY = process.env.REACT_APP_TMDB_APYKEY;
 
-const VideoDescriptionPage = ({movieId}) => {
+const VideoDescriptionPage = ({movieId, show, handleClose}) => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [movieResults, setMovieResults] = useState(null);
   
-    useEffect(() => {
+    useEffect( () => {
       const api_url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${APY_KEY}&language=es-ES`;
-      fetch(api_url)
-      .then(data => data.json())
-      .then(resultado => {
-        //console.log(resultado.original_title);
-          setMovieResults(resultado);
-          setIsLoading(false);
-        });
-    }, []);
+       fetch(api_url)
+        .then(data => data.json())
+        .then(resultado => {
+          //console.log(resultado.original_title);
+            setMovieResults(resultado);
+            setIsLoading(false);
+          });
+      
+
+    }, [movieId]);
 
     if (isLoading) {
         return (
@@ -32,12 +34,18 @@ const VideoDescriptionPage = ({movieId}) => {
       }
     
       return (
-        <div className="video-description-page-container">
-          <div className="centered-div">
-              <VideoDescription datamovie={movieResults} >
-              </VideoDescription>
-          </div>
-        </div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="video-description-page-container">
+              <div className="centered-div">
+                  <VideoDescription datamovie={movieResults} >
+                  </VideoDescription>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
       );
  }
   
