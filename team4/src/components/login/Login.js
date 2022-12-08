582331678b5b3962549905b3adfa10a0baebdd01
Fede <festerico@hotmail.com>
2022-12-08 16:47:40 -0300

    intento de avance del login :s

--- team4/src/components/login/Login.js
+++ team4/src/components/login/Login.js
@@ -1,85 +1,81 @@
-import React from 'react';
-import './Login.css';
-import ButtonLogIn from './ButtonLogIn';
-import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
-// import { firebaseApp } from '../../firebase/firebaseConfig';
-// import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import {logInWithEmail} from '../../functions/firebaseActions'
-
+import React from "react";
+import "./Login.css";
+import ButtonLogIn from "./ButtonLogIn";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { firebaseApp } from '../../firebase/firebaseConfig';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { logInWithEmail } from "../../functions/firebaseActions";
 
 function Login() {
 
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const auth = getAuth(firebaseApp);
+    const user = signInWithEmailAndPassword(auth, email, password)
+    
+    };
 
-    // function placeHolderFunction ( event) {
-    //     event.preventDefault(); // evitar la propagacion del evento..
-    //     const email = 'team4@test.com';
-    //     const password = '123456';
-    //     const auth = getAuth(firebaseApp);
-    //     signInWithEmailAndPassword(auth, email, password)
-    //     .then((credentials) => {
-    //         // Si llegamos aca... es porque se logeo..
-    //         const firebase_user = credentials.user;
-    //         console.log(firebase_user);
-    //     })
-    //     .catch( (error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //         console.log(errorCode);
-    //         console.log(errorMessage);
-    //     } );
+const [user, setUser] = useState({
+    email:"",
+    password:"",
 
-    //     console.log("placeholder");
-    // }
+});
 
-    /*BORRAR CUANDO ESTE EL LOGIN TERMINADO*/
-    const login = (e) => {
-        e.preventDefault()
-        logInWithEmail('team4@test.com', '123456' )
-    }
+const handleChange = ({target:{name,value}})=>{
+    setUser({...user,[name]: value})
 
-    return (
+};
 
+
+  return (
     <div className="flex-container LoginContainer">
-        <div className='Login'>
-         <h1 className="loginPage-title"> Login to Netflix </h1>
+      <div className="Login">
+        <h1 className="loginPage-title"> Login to Netflix </h1>
 
-            <form onSubmit={ (event) => login(event)}>
-                <h1>Inicia sesión</h1>
+        <form onSubmit={handleSubmit}>
+          <h1>Inicia sesión</h1>
 
-                <div className='InpUserName'>
-                    <input type ="text" 
-                     name="username" 
-                     placeholder='Email o número de teléfono'
-                     onChange={/*logInWithEmail()*/''}/>
-                    
-                </div>
+          <div className="InpUserName">
+            <input
+              type="text"
+              name="username"
+              placeholder="Email o número de teléfono"
+              onChange={handleChange}
+            />
+          </div>
 
-                <div className='inpPassword'>
-                  <input type="password" 
-                    name="password" 
-                     placeholder='Contraseña'
-                     onChange={/*logInWithEmail()*/'' }/>
-                     
-                </div>
-                <div>
-                    <ButtonLogIn></ButtonLogIn>   
-                </div >
-                <div className='checkBoxLogin'>
-                    <Form.Check aria-label="option 1"  label="Recuérdame" />
-                    <Link to= "/LoginHelp" >¿Necesitas ayuda?</Link>
-                </div>
-                    
-                
-            </form>
-            <div className="login-signup-now" data-uia="login-signup-now">¿Primera vez en Netflix? 
-            <p><b>Suscríbete ahora</b></p>
-            </div>
-            <span className='derechos'>Esta página está protegida por Google reCAPTCHA para comprobar que no solo eres un robot.</span>
+          <div className="inpPassword">
+            <input
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <ButtonLogIn></ButtonLogIn>
+          </div>
+          <div className="checkBoxLogin">
+            <Form.Check aria-label="option 1" label="Recuérdame" />
+            <Link to="/LoginHelp">¿Necesitas ayuda?</Link>
+          </div>
+        </form>
+
+        <div className="login-signup-now" data-uia="login-signup-now">
+          ¿Primera vez en Netflix?
+          <p>
+            <b>Suscríbete ahora</b>
+          </p>
         </div>
+        <span className="derechos">
+          Esta página está protegida por Google reCAPTCHA para comprobar que no
+          solo eres un robot.
+        </span>
+      </div>
     </div>
-
-);
-
+  );
 }
 export default Login;
