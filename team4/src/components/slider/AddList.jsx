c04b09aa0a98496910462005b26d9540ec1b8b09
mvillalva77 <maxi.villalva@gmail.com>
2022-12-15 07:15:37 -0300

    Playlist

--- team4/src/components/slider/AddList.jsx
+++ team4/src/components/slider/AddList.jsx
@@ -1,36 +1,43 @@
 import React from "react";
 import { useContext } from "react";
-import { MainContext } from "../../context/MainContext"
+import { MainContext } from "../../context/MainContext";
 import { TYPE } from "../../functions/general";
 import { updateData } from "../../functions/firebaseActions";
 import { IoAddCircleOutline } from "react-icons/io5";
 
-const AddList = ({id, media_type, name, image, genres}) => {
-
-    const {profiles, currentProfile, changeState} = useContext(MainContext)    
-    const editProfiles = profiles.filter(e => e.uuid !== currentProfile.uuid)
+const AddList = ({ id, media_type, name, image, genres }) => {
+    const { profiles, currentProfile, changeState } = useContext(MainContext);
+    const editProfiles = profiles.filter((e) => e.uuid !== currentProfile.uuid);
 
     const addMyList = async () => {
+        const playlist = currentProfile.playlist.filter((item) => item.id !== id);
 
-        const playlist = currentProfile.playlist.filter(item => item.id !== id)
-        
-        playlist.push({id: id, media_type: media_type, name:name, image: image, genres: genres})
+        playlist.push({ 
+                        id: id, 
+                        media_type: media_type, 
+                        name: name, 
+                        image: image, 
+                        genres: genres 
+                    });
 
-        currentProfile.playlist = playlist
+        currentProfile.playlist = playlist;
 
-        editProfiles.push(currentProfile)
+        editProfiles.push(currentProfile);
+        console.log(editProfiles)
 
-        await updateData('users', {profiles: editProfiles})
+        await updateData("users", { profiles: editProfiles });
 
-        changeState(TYPE.currentProfile, currentProfile)
-        changeState(TYPE.profiles, editProfiles)
-    }
+        changeState(TYPE.currentProfile, currentProfile);
+        changeState(TYPE.profiles, editProfiles);
+    };
 
     return (
         <IoAddCircleOutline
             className="me-2"
             title="Add to my list"
-            onClick={() => {addMyList();}}
+            onClick={() => {
+                addMyList();
+            }}
         />
     );
 };
